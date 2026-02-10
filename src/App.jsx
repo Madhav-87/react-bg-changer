913aec4d09633d87f8c085e2a17e10a75200fe8e
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-02-10 14:05:34 +0530

    functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [color, setColor] = useState('red')
+  function changeColor(color){
+    setColor(color)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={`min-height-width main-panel ${color}`}>
+      <div className='title-section'><span className='title-txt'>Change Background Color PlayGround</span></div>
+      <div className='button-section'>
+        <button type='button' onClick={()=>{changeColor('red')}}>Red</button>
+        <button type='button' onClick={()=>{changeColor('green')}}>Green</button>
+        <button type='button' onClick={()=>{changeColor('yellow')}}>Yellow</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
